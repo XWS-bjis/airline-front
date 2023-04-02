404f7009cdbbf114f575cea9e27a3373b82388e1
Jelena Petric <petricjelena00@gmail.com>
2023-04-02 12:58:50 +0200

    add access permission for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,16 @@ import { AllReservationComponent } from './components/reservation/all-reservatio
 import { LoginComponent } from './components/users/login/login.component';
 import { RegisterComponent } from './components/users/register/register.component';
 import { CreateFlightComponent } from './components/flight/create-flight/create-flight.component';
+import { AuthGuardService as AuthGuard } from './components/users/shared/auth-guard.service';
 
 
 const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: '', component: FlightsPageComponent},
-    {path: 'my-flights', component: AllReservationComponent},
+    {path: 'my-flights', component: AllReservationComponent, canActivate:[AuthGuard]},
     {path: 'register', component: RegisterComponent},
     {path: "flight/create-flight", component: CreateFlightComponent},
-    {path: "create-flight", component: CreateFlightComponent}
+    {path: "create-flight", component: CreateFlightComponent, canActivate:[AuthGuard]}
 ];
 
 @NgModule({
